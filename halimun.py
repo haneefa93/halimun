1e1a7689bf28f549d29947fcf22b724c2cbb3142
Abdul Somat Budiaji <abdulbudiaji@gmail.com>
2016-10-11 14:08:03 +0700

    Tambah support untuk kodepos dan cresta

--- halimun.py
+++ halimun.py
@@ -50,13 +50,86 @@ class Halimun:
             except IndexError:
                 raise RuntimeError("Info admin %s %s not found" %(geolevel, query))
 
-    def pilih_banyak_provinsi_dari_cresta(self, cresta):
-        # TODO: mapping cresta to province
-        # TODO: mapping province to cresta
-        raise NotImplementedError
+    def pilih_banyak_provinsi_dari_cresta(self, cresta, quantity):
+        """
+        expected return:
+        two list:
+        - id_admin
+        - quantity distribution for each ida_admin
+        """
+        n_init = 5
+        init_provinsi = np.zeros(n_init)
+        for i in xrange(n_init):
+            init_provinsi[i] = self.cari_provinsi_dari_cresta(cresta)
+        uniq_provinsi = np.unique(init_provinsi)
+        n_provinsi = np.alen(uniq_provinsi)
+        val_quan = quantity / np.int(n_provinsi)
+        uniq_quantity = np.zeros(n_provinsi)
+        for i in xrange(n_provinsi-1):
+            uniq_quantity[i] = val_quan
+        uniq_quantity[-1] = quantity - np.sum(uniq_quantity)
+        assert np.sum(uniq_quantity) == quantity
+        return (uniq_provinsi, uniq_quantity)
 
-    def pilih_banyak_kecamatan_dari_kodepos(self, kodepos):
-        raise NotImplementedError
+    def cari_provinsi_dari_cresta(self, id_cresta):
+        """
+        sebuah cresta dapat terdiri dari banyak provinsi
+        """
+        try:
+            keys_set = "mapping-cresta-%s-provinsi" %(int(float((id_cresta))))
+        except ValueError:
+            raise RuntimeError("Input cresta using numerical value, please.")
+        id_provinsi = self.rc.srandmember(keys_set)
+        return int(id_provinsi)
+
+    def cari_cresta_dari_provinsi(self, id_provinsi):
+        keys_hash = "mapping-provinsi-cresta"
+        keys_provinsi = "provinsi-%d" %(int(id_provinsi))
+        id_cresta = self.rc.hget(keys_hash, keys_provinsi)
+        return int(id_provinsi)
+
+    def pilih_banyak_kecamatan_dari_kodepos(self, kodepos, quantity):
+        n_init = 5
+        init_kecamatan = np.zeros(n_init)
+        for i in xrange(n_init):
+            init_kecamatan[i] = self.cari_kecamatan_dari_kodepos(kodepos)
+        uniq_kecamatan = np.unique(init_kecamatan)
+        n_kecamatan = np.alen(uniq_kecamatan)
+        val_quan = quantity / np.int(n_kecamatan)
+        uniq_quantity = np.zeros(n_kecamatan)
+        for i in xrange(n_kecamatan-1):
+            uniq_quantity[i] = val_quan
+        uniq_quantity[-1] = quantity - np.sum(uniq_quantity)
+        assert np.sum(uniq_quantity) == quantity
+        return (uniq_kecamatan, uniq_quantity)
+
+    def cari_kecamatan_dari_kodepos(self, kodepos):
+        """
+        sebuah kodepos dapat terdiri dari banyak kecamatan
+        """
+        print kodepos
+        try:
+            num_kodepos = int(float(kodepos))
+        except ValueError:
+            num_kodepos = self._hitung_kodepos_digit(kodepos)
+        keys_set = "mapping-kodepos-%d-kecamatan" %(num_kodepos)
+        id_kecamatan = self.rc.srandmember(keys_set)
+        return int(id_kecamatan)
+
+    def _hitung_kodepos_digit(self, kodepos):
+        """
+        Kodepos di Indonesia umumnya terdiri dari lima digit angka,
+        namun ada softaware tertentu yang menambahkan huruf dibelakang kodepos
+        tersebut.
+
+        Fungsi ini mengambil 4 digit paling depan kodepos
+        """
+        emp_kodepos = str(kodepos)[:4]
+        try:
+            num_kodepos = int(float(emp_kodepos))
+            return num_kodepos
+        except ValueError:
+            raise RuntimeError("Bukan format kodepos yang benar")
 
     def pilih_banyak_desa(self, geoname, geolevel, quantity):
         """
@@ -79,12 +152,12 @@ class Halimun:
 
         if geolevel == "cresta":
             div_factor = geolevel_divisor["provinsi"]
-            pilihan_provinsi = self.pilih_banyak_provinsi_dari_cresta(geoname)
+            pilihan_provinsi = self.pilih_banyak_provinsi_dari_cresta(geoname, quantity)
             daftar_id_admin = pilihan_provinsi[0]
             dist_quantity = pilihan_provinsi[1]
         elif geolevel == "kodepos":
             div_factor = geolevel_divisor["kecamatan"]
-            pilihan_kecamatan = self.pilih_banyak_kecamatan_dari_kodepos(geoname)
+            pilihan_kecamatan = self.pilih_banyak_kecamatan_dari_kodepos(geoname, quantity)
             daftar_id_admin = pilihan_kecamatan[0]
             dist_quantity = pilihan_kecamatan[1]
         else:
@@ -99,7 +172,7 @@ class Halimun:
         for i,j in zip(daftar_id_admin, dist_quantity):
             ind_id_kelurahan = np.nonzero(div_id_kelurahan==i)[0]
             fil_id_kelurahan = self.daftar_id_kelurahan_num[ind_id_kelurahan]
-            sam_id_kelurahan = np.random.choice(fil_id_kelurahan, size=j)
+            sam_id_kelurahan = np.random.choice(fil_id_kelurahan, size=np.int(j))
             temp_hasil.append(sam_id_kelurahan)
         final_hasil = np.hstack(np.array(temp_hasil))
         return (geolevel, geoname, final_hasil)
@@ -108,6 +181,6 @@ class Halimun:
         """
         geometry_desa: dapet dari redis, bentuk string wkt
         """
-        #geometry_desa = 
+        #TODO: hitung random point 
         pass
 
